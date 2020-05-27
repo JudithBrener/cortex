5106096e89661d5e80538e4b035a87d31ef23c9f
JudithBrener <judi.brener@gmail.com>
2020-05-27 21:02:23 +0300

    add parsers as service

--- cortex/parsers/parsers_manager.py
+++ cortex/parsers/parsers_manager.py
@@ -2,6 +2,8 @@ import importlib
 import inspect
 import pathlib
 
+import pika
+
 
 def get_available_parsers():
     available_parsers = {}
@@ -24,3 +26,22 @@ def run_parser(parser_name, raw_data):
     if parser_name not in parsers:
         raise NameError(f'No parser named {parser_name}. Make sure it exists in parsers package.')
     return parser_name[parser_name](raw_data)
+
+
+def consume_from_rabbitmq(parser_name, host, port):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=host, port=port))
+        channel = connection.channel()
+        channel.exchange_declare(exchange='cortex.snapshots', exchange_type='fanout')
+        result = channel.queue_declare('', exclusive=True)
+        queue_name = result.method.queue
+        channel.queue_bind(exchange='cortex.snapshots', queue=queue_name)
+
+        channel.exchange_declare(exchange="cortex.parsers", exchange_type="topic")
+
+        def callback(ch, method, properties, body):
+            parsed_message = run_parser(parser_name, body)
+            ch.basic_publish(exchange='cortex.parsers', routing_key=parser_name, body=parsed_message)
+
+        channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+        channel.start_consuming()
