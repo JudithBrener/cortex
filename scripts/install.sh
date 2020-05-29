f9b65f863148a60d1f8e2de8936b403b768585d4
JudithBrener <judi.brener@gmail.com>
2020-05-29 18:11:50 +0300

    fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ set -e
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 function main() {
-  python3 -m virtualenv .env --prompt "[cortex] "
+  python -m virtualenv .env --prompt "[cortex] "
   find .env -name site-packages -exec bash -c 'echo "../../../../" > {}/self.pth' \;
   .env/bin/pip install -U pip
   .env/bin/pip install -r requirements.txt
