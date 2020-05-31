36a181aa6b78c375b16bcdff12f9cb002302ef2b
JudithBrener <judi.brener@gmail.com>
2020-06-01 00:51:02 +0300

    fix tests

--- tests/client/test_client.py
+++ tests/client/test_client.py
@@ -1,8 +1,36 @@
+import datetime as dt
 import pathlib
+import struct
 
 import pytest
 
 from cortex.client.reader import Reader
+from cortex.proto.cortex_pb2 import User, Pose, ColorImage, DepthImage, Feelings, Snapshot
+
+
+@pytest.fixture
+def snapshot():
+    translation = Pose.Translation(x=1, y=2, z=3)
+    rotation = Pose.Rotation(x=1, y=2, z=3, w=4)
+    pose = Pose(translation=translation, rotation=rotation)
+    data = (1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1)
+    data = struct.pack('27B', *data)
+    color_image = ColorImage(width=3, height=3, data=data)
+    depth_image = DepthImage(width=3, height=3, data=[0.0, 0.0, 0.0, 1.0, 1.1, 1.0, 1.6, 1.1, 1.6])
+    feelings = Feelings(hunger=1.0, thirst=2.0, exhaustion=3.0, happiness=4.0)
+
+    return Snapshot(datetime=1590954369, pose=pose, color_image=color_image, depth_image=depth_image, feelings=feelings)
+
+
+@pytest.fixture
+def user():
+    return User(user_id=6, username="Judith Brener", birthday=int(dt.datetime(1994, 5, 1).timestamp()), gender=1)
+
+
+@pytest.fixture
+def mind_sample():
+    resources_dir = pathlib.Path(__file__).resolve().parent.parent / 'resources'
+    return resources_dir / 'test_sample.mind'
 
 
 @pytest.fixture
@@ -18,4 +46,3 @@ def test_read_user(reader, user):
 def test_read_snapshot(reader, snapshot):
     for read_snapshot in reader:
         assert read_snapshot.SerializeToString() == snapshot.SerializeToString()
-
